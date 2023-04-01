ac81d3316574c29686176e0e6815c7fd236a3ce5
Kodai-study <anchor.kou@softbank.ne.jp>
2023-04-01 14:16:17 +0900

    デバッグがVSCode上で行えるように設定情報を記述

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import SampleParts from './SampleParts';
 
 function App() {
+
+  const [count] = useState(100);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <SampleParts count={count}/>
+    <div className="App">Hello React</div>
+    </>
   );
 }
 
---
+++ src/SampleParts.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const SampleParts = (props) => {
+  return (
+    <div>
+      <h1>Sample Parts</h1>
+        <p>受け取った値: {props.count}</p>
+    </div>
+  );
+}
+
+export default SampleParts;
